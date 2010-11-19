fcdcf528f8bfd0bb474b808d222811e2009248a1
povmaniaco <p.alcaide@hotmail.com>
2010-11-19 08:23:50 +0100

    Create correct commit by SmartGit ( thanks DarkTide)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 import sys, os, threading, time
 
 PLUGIN_PATH = os.path.join(__path__[0], 'bin', 'plugins')
@@ -5,19 +22,7 @@ BIN_PATH = os.path.join(__path__[0], 'bin')
 
 sys.path.append(BIN_PATH)
 
-if sys.platform == 'win32':
-    # preload some dlls so users do not have to mess about with path
-    import ctypes
-    for dll in ['Iex','Half','IlmThread','IlmImf','mingwm10',
-                'libfreetype-6','iconv','libxml2','libtiff-3',
-                'yafaraycore', 'yafarayplugin']: # my compiled lib, from Kerbox branch
-    #for dll in ['zlib1','libxml2','pthreadVC2','yafaraycore','yafarayplugin']:
-        try:
-            ctypes.cdll.LoadLibrary(os.path.join(BIN_PATH, dll))
-        except Exception as e:
-            print("ERROR: Failed to load library " + dll + ", " + repr(e));
 
-import bpy
 
 bl_addon_info = {
     "name": "YafaRay Integration",
@@ -28,8 +33,30 @@ bl_addon_info = {
     "warning" : "VERY ALPHA!",
     "description": "YafaRay integration for blender 2.5. When activated, YafaRay will be available in the Render Engine dropdown"
     }
+if sys.platform == 'win32':
+    # preload some dlls so users do not have to mess about with path
+    import ctypes
+    for dll in ['Iex','Half','IlmThread','IlmImf','mingwm10',
+                'libfreetype-6','iconv','libxml2','libtiff-3',
+                'yafaraycore','yafarayplugin', # changed for dll compiled from Kerbox branch (msvc)
+                'pthreadVC2','zlib1','libxml2']: # add all dlls
+    #for dll in ['zlib1','libxml2','pthreadVC2','yafaraycore','yafarayplugin']:
+        try:
+            ctypes.cdll.LoadLibrary(os.path.join(BIN_PATH, dll))
+        except Exception as e:
+            print("ERROR: Failed to load library " + dll + ", " + repr(e));
+ 
 
-def register():
+#from yafaray import core
+import bpy
+
+from yafaray import io
+from yafaray import ui
+from yafaray import op
+
+def register(): # add "style Campbell Barton" from Povray engine
+    Scene = bpy.types.Scene
+"""    
     import io, ui, op
     io.register()
     for submodule in [ui, op]:
@@ -40,6 +67,7 @@ def register():
                  pass
 
     return
+"""
 """
     try:
         import io, ui, op
@@ -63,8 +91,11 @@ def register():
 """
     
 def unregister():
-    pass
+    import bpy
+    Scene = bpy.types.Scene
 """
+    pass
+
     import io, ui, op
     io.unregister()
     for submodule in [ui, op]:
