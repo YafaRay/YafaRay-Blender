666acbb2656eb54eca94749b50fc4ffed3258205
povmaniaco <p.alcaide@hotmail.com>
2010-12-09 15:37:43 -0800

    Update all 09/12.

--- ui/properties_yaf_AA_settings.py
+++ ui/properties_yaf_AA_settings.py
@@ -1,34 +1,30 @@
 import bpy
+from bpy.props import *
+Scene = bpy.types.Scene
 
-"""
-FloatProperty = bpy.types.Scene.FloatProperty
-IntProperty = bpy.types.Scene.IntProperty
-BoolProperty = bpy.types.Scene.BoolProperty
-CollectionProperty = bpy.types.Scene.CollectionProperty
-EnumProperty = bpy.types.Scene.EnumProperty
-FloatVectorProperty = bpy.types.Scene.FloatVectorProperty
-StringProperty = bpy.types.Scene.StringProperty
-IntVectorProperty = bpy.types.Scene.IntVectorProperty
-"""
 
-bpy.types.Scene.AA_min_samples = bpy.props.IntProperty(attr="AA_min_samples",
-		min = 1, default = 1)
-bpy.types.Scene.AA_inc_samples = bpy.props.IntProperty(attr="AA_inc_samples",
-		min = 1, default = 1)
-bpy.types.Scene.AA_passes = bpy.props.IntProperty(attr="AA_passes",
-		min = 1, default = 1)
-bpy.types.Scene.AA_threshold = bpy.props.FloatProperty(attr="AA_threshold",
-		min = 0.0, max = 1.0, default = 0.05, precision = 4)
-bpy.types.Scene.AA_pixelwidth = bpy.props.FloatProperty(attr="AA_pixelwidth",
-		min = 1, default = 1.5)
-bpy.types.Scene.AA_filter_type = bpy.props.EnumProperty(attr="AA_filter_type",
-	items = (
-		("AA Filter Type","AA Filter Type",""),
-		("Box","Box",""),
-		("Mitchell","Mitchell",""),
-		("Gauss","Gauss",""),
-		("Lanczos","Lanczos",""),
-),default="Box")
+Scene.AA_min_samples = IntProperty(attr="AA_min_samples",
+                        description = "Minin samples for pase",
+                        default = 1)
+Scene.AA_inc_samples = IntProperty(attr="AA_inc_samples",
+                        description = "Increment samples per passes",
+                        default = 1)
+Scene.AA_passes =      IntProperty(attr="AA_passes",
+                        description = "Number of passes",
+                        default = 1)
+Scene.AA_threshold =   FloatProperty(attr="AA_threshold",
+                        description = "",
+                        default = 0.05, precision = 4)
+Scene.AA_pixelwidth =  FloatProperty(attr="AA_pixelwidth",
+                        description = "",
+                        default = 1.5)
+Scene.AA_filter_type = EnumProperty(attr="AA_filter_type",
+                    items = (
+                    ("AA Filter Type","AA Filter Type",""),
+                    ("Box","Box",""),
+                    ("Mitchell","Mitchell",""),
+                    ("Gauss","Gauss",""),
+                    ),default="Gauss")
 
 
 class YAF_PT_AA_settings(bpy.types.Panel):
@@ -41,27 +37,25 @@ class YAF_PT_AA_settings(bpy.types.Panel):
 
 	@classmethod
 	def poll(self, context):
-
 		engine = context.scene.render.engine
 		return (True  and  (engine in self.COMPAT_ENGINES) ) 
 
 
 	def draw(self, context):
-		
-		self.list = [1,2,4,5]
+
 		layout = self.layout
 		split = layout.split()
-
 		col = split.column()
-		col.prop(context.scene,"AA_filter_type", text= "Filter Type")
+
 		col.prop(context.scene,"AA_min_samples", text= "AA Samples")
-		col.prop(context.scene,"AA_pixelwidth", text= "AA Pixelwidth")
-
-		col = split.column()
+		col.prop(context.scene,"AA_inc_samples", text= "AA Inc. Samples")
 		col.prop(context.scene,"AA_passes", text= "AA Passes")
-		if context.scene.AA_passes > 1:
-			col.prop(context.scene,"AA_inc_samples", text= "AA Inc. Samples")
-			col.prop(context.scene,"AA_threshold", text= "AA Threshold")
+		col = split.column()
+		col.prop(context.scene,"AA_threshold", text= "AA Threshold")
+		col.prop(context.scene,"AA_pixelwidth", text= "AA Pixelwidth")
+		col.prop(context.scene,"AA_filter_type", text= "AA Filter Type")
+
+
 
 
 classes = [
@@ -82,3 +76,4 @@ def unregister():
 
 if __name__ == "__main__":
 	register()
+
