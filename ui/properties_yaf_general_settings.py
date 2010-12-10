1d289eac4109b1d5563f50a01965d9e21ed27699
povmaniaco <p.alcaide@hotmail.com>
2010-12-10 03:10:01 -0800

    Update all 10/12.

--- ui/properties_yaf_general_settings.py
+++ ui/properties_yaf_general_settings.py
@@ -1,46 +1,65 @@
 import bpy
+# import all props 
+from bpy.props import *
 
-"""
-FloatProperty = bpy.types.Scene.FloatProperty
-IntProperty = bpy.types.Scene.IntProperty
-BoolProperty = bpy.types.Scene.BoolProperty
-CollectionProperty = bpy.types.Scene.CollectionProperty
-EnumProperty = bpy.types.Scene.EnumProperty
-FloatVectorProperty = bpy.types.Scene.FloatVectorProperty
-StringProperty = bpy.types.Scene.StringProperty
-IntVectorProperty = bpy.types.Scene.IntVectorProperty
-"""
+Scene = bpy.types.Scene
 
-bpy.types.Scene.gs_ray_depth = bpy.props.IntProperty(attr="gs_ray_depth",
-		default = 2)
-bpy.types.Scene.gs_shadow_depth = bpy.props.IntProperty(attr="gs_shadow_depth",
-		default = 2)
-bpy.types.Scene.gs_threads = bpy.props.IntProperty(attr="gs_threads",
-		default = 1)
-bpy.types.Scene.gs_gamma = bpy.props.FloatProperty(attr="gs_gamma",
-		default = 1.8)
-bpy.types.Scene.gs_gamma_input = bpy.props.FloatProperty(attr="gs_gamma_input",
-		default = 1.8)
-bpy.types.Scene.gs_tile_size = bpy.props.IntProperty(attr="gs_tile_size",
-		default = 32)
-bpy.types.Scene.gs_tile_order = bpy.props.EnumProperty(attr="gs_tile_order",
+Scene.gs_ray_depth =    IntProperty(attr="gs_ray_depth",
+                        description = "Depth of Raytrace", # for description tooltips
+                        default = 2)
+Scene.gs_shadow_depth = IntProperty(attr="gs_shadow_depth",
+                        description = "",
+                        default = 2)
+Scene.gs_threads =      IntProperty(attr="gs_threads",
+                        description = "",
+                        default = 1)
+Scene.gs_gamma =        FloatProperty(attr="gs_gamma",
+                        default = 1.8)
+Scene.gs_gamma_input =  FloatProperty(attr="gs_gamma_input",
+                        description = "",
+                        default = 1.8)
+Scene.gs_tile_size =    IntProperty(attr="gs_tile_size",
+                        description = "",
+                        default = 32)
+Scene.gs_tile_order =   EnumProperty(attr="gs_tile_order",
 	items = (
 		("Tile order","Tile order",""),
 		("Linear","Linear",""),
 		("Random","Random",""),
 ),default="Random")
-bpy.types.Scene.gs_auto_threads = bpy.props.BoolProperty(attr="gs_auto_threads", default = True)
-bpy.types.Scene.gs_clay_render = bpy.props.BoolProperty(attr="gs_clay_render")
-bpy.types.Scene.gs_draw_params = bpy.props.BoolProperty(attr="gs_draw_params")
-bpy.types.Scene.gs_custom_string = bpy.props.StringProperty(attr="gs_custom_string")
-bpy.types.Scene.gs_auto_save = bpy.props.BoolProperty(attr="gs_auto_save")
-bpy.types.Scene.gs_auto_alpha = bpy.props.BoolProperty(attr="gs_auto_alpha")
-bpy.types.Scene.gs_premult = bpy.props.BoolProperty(attr="gs_premult")
-bpy.types.Scene.gs_transp_shad = bpy.props.BoolProperty(attr="gs_transp_shad")
-bpy.types.Scene.gs_clamp_rgb = bpy.props.BoolProperty(attr="gs_clamp_rgb")
-bpy.types.Scene.gs_show_sam_pix = bpy.props.BoolProperty(attr="gs_show_sam_pix")
-bpy.types.Scene.gs_z_channel = bpy.props.BoolProperty(attr="gs_z_channel")
-
+Scene.gs_auto_threads = BoolProperty(attr="gs_auto_threads",
+                        description = "", 
+                        default = True)
+Scene.gs_clay_render =  BoolProperty(attr="gs_clay_render",
+                        description = "",
+                        default = False)
+Scene.gs_draw_params =  BoolProperty(attr="gs_draw_params",
+                        description = "",
+                        default = False)
+Scene.gs_custom_string = StringProperty(attr="gs_custom_string",
+                        description = "",
+                        default = "")
+Scene.gs_auto_save =    BoolProperty(attr="gs_auto_save",
+                        description = "",
+                        default = False)
+Scene.gs_auto_alpha =   BoolProperty(attr="gs_auto_alpha",
+                        description = "",
+                        default = False)
+Scene.gs_premult =      BoolProperty(attr="gs_premult",
+                        description = "",
+                        default = False)
+Scene.gs_transp_shad =  BoolProperty(attr="gs_transp_shad",
+                        description = "",
+                        default = False)
+Scene.gs_clamp_rgb =    BoolProperty(attr="gs_clamp_rgb",
+                        description = "",
+                        default = False)
+Scene.gs_show_sam_pix = BoolProperty(attr="gs_show_sam_pix",
+                        description = "",
+                        default = False)
+Scene.gs_z_channel =    BoolProperty(attr="gs_z_channel",
+                        description = "",
+                        default = False)
 
 class YAF_PT_general_settings(bpy.types.Panel):
 
@@ -57,6 +76,7 @@ class YAF_PT_general_settings(bpy.types.Panel):
 		return (True  and  (engine in self.COMPAT_ENGINES) ) 
 
 
+
 	def draw(self, context):
 
 		layout = self.layout
@@ -65,9 +85,7 @@ class YAF_PT_general_settings(bpy.types.Panel):
 
 		col.prop(context.scene,"gs_ray_depth", text= "Ray Depth")
 		col.prop(context.scene,"gs_shadow_depth", text= "Shadow Depth")
-		if not context.scene.gs_auto_threads:
-			col.prop(context.scene,"gs_threads", text= "Threads")
-		
+		col.prop(context.scene,"gs_threads", text= "Threads")
 		col.prop(context.scene,"gs_gamma", text= "Gamma")
 		col.prop(context.scene,"gs_gamma_input", text= "Gamma Input")
 		col.prop(context.scene,"gs_tile_size", text= "Tile Size")
@@ -88,16 +106,11 @@ class YAF_PT_general_settings(bpy.types.Panel):
 
 		col.prop(context.scene,"gs_clamp_rgb", text= "Clamp RGB")
 
-		#col.prop(context.scene,"gs_show_sam_pix", text= "Show Sam Pix")
-		
+		col.prop(context.scene,"gs_show_sam_pix", text= "Show Sam Pix")
 		col.prop(context.scene,"gs_draw_params", text= "Draw Params")
-		
-		col.prop(context.scene,"gs_z_channel", text= "Render Z-buffer")
 		if context.scene.gs_draw_params:
-			row = layout.row()	
+			row = layout.row()
 			row.prop(context.scene,"gs_custom_string", text= "Custom String")
-
-
 
 
 classes = [
