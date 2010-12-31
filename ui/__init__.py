bb686715948873969bb2879206722440d3b52fdd
povmaniaco <p.alcaide@hotmail.com>
2010-12-31 10:44:42 +0100

    Clear small code

--- ui/__init__.py
+++ ui/__init__.py
@@ -9,10 +9,7 @@ from yafaray.ui import properties_yaf_texture
 from yafaray.ui import properties_yaf_volume_integrator
 from yafaray.ui import properties_yaf_world
 from yafaray.ui import properties_yaf_AA_settings
+from yafaray.ui import properties_yaf_object_light #add
 from yafaray.ui import yaf_light
 
-#try:
-#    import yaf_properties
-#except Exception as e:
-#    import sys, traceback
-#    traceback.print_exception(*sys.exc_info())
+
