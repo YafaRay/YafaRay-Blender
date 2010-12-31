2907a3ae189370462be11dd6ae6cbf868f381009
povmaniaco <p.alcaide@hotmail.com>
2010-12-31 10:58:00 +0100

    Clear lines not used

--- io/__init__.py
+++ io/__init__.py
@@ -4,21 +4,23 @@ from yafaray.io import yaf_export
 #from yafaray import yaf_properties
 from yafaray.io import yaf_object
 
-# register engine and panels
 classes = [
     yaf_export.YafaRayRenderEngine,
     #yaf_export.YafaRayCameraPoll,
     #yaf_export.YafaRayCameraButtonsPanel
     ]
 
+
 def register():
+
     register = bpy.types.register
     for cls in classes:
         register(cls)
 
-def unregister():    
+def unregister():
     unregister = bpy.types.unregister
     for cls in classes:
         unregister(cls)
 
 #yaf_properties.yaf_register_camera_types()
+
