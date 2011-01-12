52a34d4a77107c3d19e3b03db88dd36be3e90e84
povmaniaco <p.alcaide@hotmail.com>
2011-01-13 00:57:22 +0100

    Import al files from folder /io for register all classes

--- io/__init__.py
+++ io/__init__.py
@@ -1,13 +1,26 @@
 import bpy
-#//----/ added path complet /------>
+# import al files for register all classes
+
 from yafaray.io import yaf_export
-#from yafaray import yaf_properties
+from yafaray.io import yaf_world
+from yafaray.io import yaf_general_AA
+from yafaray.io import yaf_integrator
+from yafaray.io import yaf_texture
 from yafaray.io import yaf_object
+from yafaray.io import yaf_light
+from yafaray.io import yaf_material
+import yafrayinterface
 
 classes = [
     yaf_export.YafaRayRenderEngine,
-    #yaf_export.YafaRayCameraPoll,
-    #yaf_export.YafaRayCameraButtonsPanel
+    yaf_world.yafWorld,
+    yaf_general_AA.yafGeneralAA,
+    yaf_integrator.yafIntegrator,
+    yaf_texture.yafTexture,
+    yaf_object.yafObject,
+    yaf_light.yafLight,
+    yaf_material.yafMaterial,
+
     ]
 
 
@@ -22,5 +35,4 @@ def unregister():
     for cls in classes:
         unregister(cls)
 
-#yaf_properties.yaf_register_camera_types()
 
