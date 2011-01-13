818bab5884a3c6d8fbc9240d77eca116f890cca9
povmaniaco <p.alcaide@hotmail.com>
2011-01-13 01:06:34 +0100

    Moved import properties_render and  properties_particle to /ui/__init__.py

--- io/yaf_export.py
+++ io/yaf_export.py
@@ -28,7 +28,7 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
     bl_label = "YafaRay Render"
     progress = 0.0
     tag = ""
-    
+
     def setInterface(self, yi):
         self.materialMap = {}
         self.materials   = set()
@@ -106,7 +106,7 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
         try:
             mat1_name =  mat.mat_material_one
             mat1      =  bpy.data.materials[mat1_name]
-            
+
             mat2_name =  mat.mat_material_two
             mat2      =  bpy.data.materials[mat2_name]
         except:
@@ -138,7 +138,7 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
         self.yi.printInfo("Exporter: Creating Material \"defaultMat\"")
         ymat = self.yi.createMaterial("defaultMat")
         self.materialMap["default"] = ymat
-        
+
         for mat in bpy.data.materials:
             if mat in self.materials : continue
             self.exportMaterial(mat)
@@ -154,7 +154,7 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
                 self.materials.add(material)
                 self.yaf_material.writeMaterial(material)
 
-    
+
     #    , w, h
     def configureRender(self):
         # Integrators
@@ -174,16 +174,16 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
         extension = '.' + filetype
         output = tempfile.mktemp(dir = output_path)
         outputFile = output + extension
-        
+
         return outputFile,output,filetype
-    
+
     def dummy(self):
         return self.yaf_general_aa.getRenderCoords(self.scene)
 
     # callback to render scene
     def render(self, scene):
         self.update_stats("", "Setting up render")
-            
+
         scene.frame_set(scene.frame_current)
         self.scene = scene
         r = scene.render
@@ -191,22 +191,22 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
         # compute resolution
         x= int(r.resolution_x*r.resolution_percentage*0.01)
         y= int(r.resolution_y*r.resolution_percentage*0.01)
-    
+
         self.setInterface(yafrayinterface.yafrayInterface_t())
-        
+
         outputFile,output,file_type = self.decideOutputFileName(r.filepath, r.file_format)
-                    
+
         self.yi.paramsClearAll()
         self.yi.paramsSetString("type", file_type)
         self.yi.paramsSetInt("width", x)
         self.yi.paramsSetInt("height", y)
         self.yi.paramsSetBool("alpha_channel", False)
         self.yi.paramsSetBool("z_channel", scene.gs_z_channel)
-                    
+
         self.yi.startScene()
         self.exportScene()
         self.configureRender()
-        
+
         def prog_callback(command, *args):
             if not self.test_break():
                 if command == "tag":
@@ -214,7 +214,7 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
                 elif command == "progress":
                     self.progress = args[0]
                 self.update_stats("", "%s - %.2f %%" % (self.tag, self.progress))
-        
+
         def tile_callback(command, *args):
             if command == "highliteArea":
                 x0, y0, x1, y1, tile = args
@@ -243,52 +243,55 @@ class YafaRayRenderEngine(bpy.types.RenderEngine):
                     print("Exception in flush callback: ", e)
                     print(args, len(tile))
                 self.update_result(res)
-                
+
         self.yi.paramsSetString("type", file_type)
         ih = self.yi.createImageHandler("outFile")
         co = yafrayinterface.imageOutput_t(ih, str(outputFile), 0, 0)
-                        
+
         self.yi.printInfo("Exporter: Rendering to file " + outputFile)
-                    
+
         # here we export blender scene and renders using yafaray
         if scene.gs_type_render == "file":
             result = self.begin_result(0, 0, x, y)
             lay = result.layers[0]
             self.update_stats("", "Rendering to %s" % outputFile)
             print("Rendering to %s" % outputFile)
-        
+
             self.yi.render(co)
-        
+
             if scene.gs_z_channel:
                 lay.load_from_file(output + '_zbuffer.' + file_type)
             else:
                 lay.load_from_file(outputFile)
         # done
             self.end_result(result)
-    
+
         if scene.gs_type_render == "into_blender":
             import threading
             t = threading.Thread(target=self.yi.render, args=(x, y, tile_callback, prog_callback))
             t.start()
-        
+
             while t.isAlive() and not self.test_break():
                 time.sleep(0.2)
-            
+
             if t.isAlive():
                 self.update_stats("", "Aborting...")
                 self.yi.abort()
                 t.join()
                 self.update_stats("", "Render is aborted")
                 return
-            
+
         self.update_stats("", "Done!")
 
 # Use some of the existing buttons.
+# moved to /ui/__init__.py
+"""
 import properties_render, properties_particle
 
 for panel in [properties_render.RENDER_PT_render,
               properties_render.RENDER_PT_dimensions,
               properties_render.RENDER_PT_output,
+              properties_render.RENDER_PT_shading,
               properties_particle.PARTICLE_PT_context_particles,
               properties_particle.PARTICLE_PT_emission,
               properties_particle.PARTICLE_PT_hair_dynamics,
@@ -300,5 +303,6 @@ for panel in [properties_render.RENDER_PT_render,
               properties_particle.PARTICLE_PT_draw,
               properties_particle.PARTICLE_PT_force_fields]:
     panel.COMPAT_ENGINES.add(IDNAME)
-    
+
 del properties_render, properties_particle
+"""
