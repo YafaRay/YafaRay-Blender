424b654a55afaaad838fd1e2ac9bfa8b8c5463b9
DarkTide <darknesstide@gmail.com>
2011-01-29 05:31:24 -0600

    - Cleanup continued

--- io/yaf_config.py
+++
@@ -1,2 +0,0 @@
-PLUGIN_PATH = "/usr/local/lib/yafaray"
-PATH_TO_ADD = "/home/shuvro/Desktop/blender_6_6_10/blender-svn/install/linux2/.blender/python/lib/python3.1/site-packages"
---
+++ ui/properties_yaf_render_control.py
@@ -0,0 +1,37 @@
+import bpy
+
+class RENDER_PT_render(bpy.types.Panel):
+
+    bl_label = 'Render'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'render'
+    COMPAT_ENGINES = ['YAFA_RENDER']
+    
+    @classmethod
+    def poll(self, context):
+        return (context.scene.render.engine in self.COMPAT_ENGINES)
+
+    def draw(self, context):
+
+        split = self.layout.split()
+
+        split.column().operator("RENDER_OT_render", "Render Image", "RENDER_STILL")
+        
+        props = split.column().operator("RENDER_OT_render", "Render Animation", "RENDER_ANIMATION")
+        props.animation = True
+        
+        if context.scene.render.engine == "YAFA_RENDER":
+            self.layout.row().operator("RENDER_OT_render_view", "Render 3D View", "VIEW3D")
+        
+        self.layout.row().prop(context.scene.render, "display_mode")
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
+
