5d4ae62630599cdf00bc4154f6887af3d2004c6c
DarkTide <darknesstide@gmail.com>
2011-02-11 16:30:43 -0600

    - Refactor of ui.__init__

--- ui/__init__.py
+++ ui/__init__.py
@@ -17,27 +17,31 @@ from yafaray.ui import properties_yaf_convert
 
 import properties_particle, properties_data_mesh
 
-for panel in [properties_particle.PARTICLE_PT_context_particles,
-              properties_particle.PARTICLE_PT_emission,
-              properties_particle.PARTICLE_PT_hair_dynamics,
-              properties_particle.PARTICLE_PT_velocity,
-              properties_particle.PARTICLE_PT_rotation,
-              properties_particle.PARTICLE_PT_physics,
-              properties_particle.PARTICLE_PT_boidbrain,
-              properties_particle.PARTICLE_PT_render,
-              properties_particle.PARTICLE_PT_draw,
-              properties_particle.PARTICLE_PT_force_fields,
-              properties_data_mesh.DATA_PT_context_mesh,
-              properties_data_mesh.DATA_PT_custom_props_mesh,
-              properties_data_mesh.DATA_PT_normals,
-              properties_data_mesh.DATA_PT_settings,
-              properties_data_mesh.DATA_PT_shape_keys,
-              properties_data_mesh.DATA_PT_texface,
-              properties_data_mesh.DATA_PT_uv_texture,
-              properties_data_mesh.DATA_PT_vertex_colors,
-              properties_data_mesh.DATA_PT_vertex_groups,
-              properties_data_mesh.MESH_MT_shape_key_specials,
-              properties_data_mesh.MESH_MT_vertex_group_specials]:
+panelSet = [
+properties_particle.PARTICLE_PT_context_particles,
+properties_particle.PARTICLE_PT_emission,
+properties_particle.PARTICLE_PT_hair_dynamics,
+properties_particle.PARTICLE_PT_velocity,
+properties_particle.PARTICLE_PT_rotation,
+properties_particle.PARTICLE_PT_physics,
+properties_particle.PARTICLE_PT_boidbrain,
+properties_particle.PARTICLE_PT_render,
+properties_particle.PARTICLE_PT_draw,
+properties_particle.PARTICLE_PT_force_fields,
+properties_data_mesh.DATA_PT_context_mesh,
+properties_data_mesh.DATA_PT_custom_props_mesh,
+properties_data_mesh.DATA_PT_normals,
+properties_data_mesh.DATA_PT_settings,
+properties_data_mesh.DATA_PT_shape_keys,
+properties_data_mesh.DATA_PT_texface,
+properties_data_mesh.DATA_PT_uv_texture,
+properties_data_mesh.DATA_PT_vertex_colors,
+properties_data_mesh.DATA_PT_vertex_groups,
+properties_data_mesh.MESH_MT_shape_key_specials,
+properties_data_mesh.MESH_MT_vertex_group_specials
+]
+
+for panel in panelSet:
     panel.COMPAT_ENGINES.add('YAFA_RENDER')
 
 del properties_particle, properties_data_mesh
