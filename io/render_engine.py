8d4799cfac603657f3cbc828f38762d06a2b4991
DavidBluecame <david.bluecame@gmail.com>
2023-10-21 07:29:56 +0200

    Got to rendering state (to a file for now!) :-)

--- io/render_engine.py
+++ io/render_engine.py
@@ -56,14 +56,74 @@ class RenderEngine(bpy.types.RenderEngine):
         scene = SceneControl(self.is_preview, depsgraph, scene_yafaray, logger)
         scene.export_scene()
 
+
     # Blender callback. Render scene into an image
     # This is the method called by Blender for both final renders (F12) and
     # small preview for materials, world and lights.
     def render(self, depsgraph):
         scene_blender = scene_from_depsgraph(depsgraph)
+        if self.is_preview:
+            scene_yafaray = scene_preview
+            logger = logger_preview
+        else:
+            scene_yafaray = scene_render
+            logger = logger_render
         scale = scene_blender.render.resolution_percentage / 100.0
         size_x = int(scene_blender.render.resolution_x * scale)
         size_y = int(scene_blender.render.resolution_y * scale)
+
+        param_map = libyafaray4_bindings.ParamMap()
+        # Creating SurfaceIntegrator #
+        param_map.clear()
+        param_map.set_string("type", "photonmapping")
+        # param_map.set_int("AA_minsamples",  50)
+        param_map.set_int("AA_passes", 5)
+        param_map.set_int("AA_inc_samples", 2)
+        param_map.set_float("AA_threshold", 0.05)
+        param_map.set_int("threads", -1)
+        param_map.set_int("threads_photons", -1)
+        surface_integrator = libyafaray4_bindings.SurfaceIntegrator(logger, "SurfaceIntegrator1", param_map)
+
+        # Creating volume integrator #
+        param_map.clear()
+        param_map.set_string("type", "none")
+        surface_integrator.define_volume_integrator(scene_yafaray, param_map)
+
+        # Creating Film #
+        param_map.clear()
+        film = libyafaray4_bindings.Film(logger, surface_integrator, "Film1", param_map)
+
+        # Creating camera #
+        param_map.clear()
+        param_map.set_string("type", "perspective")
+        param_map.set_int("resx", size_x)
+        param_map.set_int("resy", size_y)
+        param_map.set_float("focal", 1.1)
+        param_map.set_vector("from", 8.6, -7.2, 8.1)
+        param_map.set_vector("to", 8.0, -6.7, 7.6)
+        param_map.set_vector("up", 8.3, -6.8, 9)
+        film.define_camera(param_map)
+
+        # Creating image output #
+        param_map.clear()
+        param_map.set_string("image_path", "./test01-output1.tga")
+        film.create_output("output1_tga", param_map)
+
+        # Creating RenderControl #
+        render_control = libyafaray4_bindings.RenderControl()
+
+        # Creating RenderMonitor #
+
+        def monitor_callback(steps_total, steps_done, tag):
+            print("*PYTHON MONITOR CALLBACK*", steps_total, steps_done, tag)
+
+        render_monitor = libyafaray4_bindings.RenderMonitor(monitor_callback)
+
+        render_control.set_for_normal_start()
+        scene_modified_flags = scene_yafaray.check_and_clear_modified_flags()
+        scene_yafaray.preprocess(render_control, scene_modified_flags)
+        surface_integrator.preprocess(render_monitor, render_control, scene_yafaray)
+        surface_integrator.render(render_control, render_monitor, film)
 
         if self.is_preview:
             self.render_preview(scene_blender, size_x, size_y)
